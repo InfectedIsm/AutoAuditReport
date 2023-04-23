424098951fb908f561fc5521cd207c37456a44b7
InfectedIsm <52218361+InfectedIsm@users.noreply.github.com>
2023-04-23 23:04:41 +0200

    cleaned findings.ts

--- src/utils/findings.ts
+++ src/utils/findings.ts
@@ -1,46 +1,69 @@
+
+/**
+* Object representing a finding
+* A finding is an audit tag with its description
+* There can be multiple occurences of the same finding in the codebase
+* each occurence is represented by a code snippet
+*/
 export type Finding = {
-  severity: string;
-  auditTag: string;
+  auditKey: string;
   description: string;
   occurence: number;
   code: CodeSnippet[];
 };
 
+/**
+* Object representing a code snippet and its relative position in the file in regard to the audit tag
+*/
 type CodeSnippet = {
-  lineStart: number;
-  lineEnd: number;
+  tagLine: number;      
+  relativeStart: number;//relative to the audit tag
+  relativeEnd: number;  //relative to the audit tag
   codeSnippet: string;
 };
 
-type FindingsBySeverity = {
+/**
+* TODO: can be removed, redundant of key of AuditMap
+*/
+export type FindingsBySeverity = {
   severity: string;
-  findings: Finding[];
+  findings: Finding;
 };
 
+/**
+* Map of all findings
+*/
 export type FindingsMap = {
   [auditKey: string]: Finding;
 };
 
+/**
+* Used to easily iterate over the severities to generate the report
+*/
 export type AuditMap = {
-  //key: severity
-  [auditKey: string]: FindingsBySeverity[];
+  [severity: string]: FindingsBySeverity[];
 };
 
-export const extractCodeFromLines = ( lines: string[], lineStart: number, lineEnd: number, offset: number): string => {
+/**
+* Extract the code snippet from the lines array
+* @param lines: array of string representing the file
+* @param offset: line where the audit tag is
+* @param lineStart: start line relative to offset
+* @param lineEnd: end line relative to offset
+* @returns the code snippet
+*/
+export const extractCodeFromLines = ( lines: string[], offset: number, lineStart: number, lineEnd: number): string => {
   return lineStart===lineEnd ?
     lines[offset+lineStart]
-    :lines.slice(offset+ lineStart, offset+lineEnd).join("\n");
+    :lines.slice(offset+ lineStart, offset+lineEnd+1).join("\n");
 };
 
-
-export type AuditTagData = {
-  auditKey: string;
-  description: string;
-  lineStart: number;
-  lineEnd: number;
-};
-
-//array matching key with severity (ex: GXX = Gas, NCXX = Non Critical, LXX = Low, MXX = Medium, HXX = High)
+//
+/**
+ * array matching key with severity (ex: GXX = Gas, NCXX = Non Critical, LXX = Low, MXX = Medium, HXX = High)
+ * @param auditKey: string representing the audit key
+ * @returns string representing the severity
+*/
 export const auditKeyToSeverity = (auditKey: string): string => {
   const prefix = auditKey.slice(0, 1);
 
